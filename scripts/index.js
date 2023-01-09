b04ec4516e0ffaed449e4541f5f99aa8cdb699f7
Tatiana Kholod <tkholod@yandex.ru>
2023-01-09 15:23:10 +0300

    fix(popup): Исправление замечаний к попап

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,33 @@
+//данные профиля
 let profileContainer = document.querySelector('.profile__info-container');
 let editButton = profileContainer.querySelector('.profile__edit-button');
+let profileName = profileContainer.querySelector('.profile__name');
+let profileJob = profileContainer.querySelector('.profile__job');
 
+//данные попап
+let divPopup = document.querySelector('.popup');
+let closeButton = divPopup.querySelector('.popup__button-close');
 // Находим форму в DOM
-let formPopup = document.querySelector('.popup');
+let formPopup = document.querySelector('.popup__form');
 // Находим поля формы в DOM
-let closeButton = formPopup.querySelector('.popup__button-close');
-let nameInput = formPopup.querySelector('.popup__profile-info[name="profile-name"]');
-let jobInput = formPopup.querySelector('.popup__profile-info[name="profile-info"]');
+let nameInput = formPopup.querySelector('.popup__profile-info[name="name"]');
+let jobInput = formPopup.querySelector('.popup__profile-info[name="job"]');
+console.log(nameInput);
 
 function displayPopup() {
-  let profileName = profileContainer.querySelector('.profile__name');
-  let profileJob = profileContainer.querySelector('.profile__job');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
-  formPopup.classList.add('popup_opened');
+  divPopup.classList.add('popup_opened');
 }
 
 function ClosePopup() {
-  formPopup.classList.remove('popup_opened');
+  divPopup.classList.remove('popup_opened');
 }
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function handleFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Выберите элементы, куда должны быть вставлены значения полей
-  let profileName = profileContainer.querySelector('.profile__name');
-  let profileJob = profileContainer.querySelector('.profile__job');
-  // Вставьте новые значения с помощью textContent
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   ClosePopup();
